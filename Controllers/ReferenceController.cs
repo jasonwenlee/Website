cca4cb0da22df6ac881ecbec5adbe7553f97a137
Jason <jasonleewl1994@gmail.com>
2020-07-06 19:50:42 +0930

    Update to website

--- Controllers/ReferenceController.cs
+++ Controllers/ReferenceController.cs
@@ -18,21 +18,21 @@ namespace Website.Controllers
         string urlPath = WebConfigurationManager.AppSettings["apiReferencesPath"];
 
         // GET: References
-        [HttpGet]
-        public async Task<ActionResult> Index()
-        {
-            List<reference> referenceInfo = new List<reference>();
-            // Send request to find web api REST service
-            HttpResponseMessage response = await client.GetAsync(urlPath);
+        //[HttpGet]
+        //public async Task<ActionResult> Index()
+        //{
+        //    List<reference> referenceInfo = new List<reference>();
+        //    // Send request to find web api REST service
+        //    HttpResponseMessage response = await client.GetAsync(urlPath);
 
-            // Check if response is successful 
-            if (response.IsSuccessStatusCode)
-            {
-                var responseDetail = response.Content.ReadAsStringAsync().Result;
-                referenceInfo = JsonConvert.DeserializeObject<List<reference>>(responseDetail);
-            }
-            return View(referenceInfo);
-        }
+        //    // Check if response is successful 
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var responseDetail = response.Content.ReadAsStringAsync().Result;
+        //        referenceInfo = JsonConvert.DeserializeObject<List<reference>>(responseDetail);
+        //    }
+        //    return View(referenceInfo);
+        //}
 
         // Get index in Json format
         [HttpGet]
