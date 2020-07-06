2aa4a776ba10059b7eaaff0d0af691554d61630f
Jason <jasonleewl1994@gmail.com>
2020-07-06 19:50:42 +0930

    Update to website

--- Controllers/KeyPointController.cs
+++ Controllers/KeyPointController.cs
@@ -18,21 +18,21 @@ namespace Website.Controllers
         string urlPath = WebConfigurationManager.AppSettings["apiKeyPointsPath"];
 
         // GET: KeyPoints
-        [HttpGet]
-        public async Task<ActionResult> Index()
-        {
-            List<keypoint> keyPointInfo = new List<keypoint>();
-            // Send request to find web api REST service
-            HttpResponseMessage response = await client.GetAsync(urlPath);
+        //[HttpGet]
+        //public async Task<ActionResult> Index()
+        //{
+        //    List<keypoint> keyPointInfo = new List<keypoint>();
+        //    // Send request to find web api REST service
+        //    HttpResponseMessage response = await client.GetAsync(urlPath);
 
-            // Check if response is successful 
-            if (response.IsSuccessStatusCode)
-            {
-                var responseDetail = response.Content.ReadAsStringAsync().Result;
-                keyPointInfo = JsonConvert.DeserializeObject<List<keypoint>>(responseDetail);
-            }
-            return View(keyPointInfo);
-        }
+        //    // Check if response is successful 
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var responseDetail = response.Content.ReadAsStringAsync().Result;
+        //        keyPointInfo = JsonConvert.DeserializeObject<List<keypoint>>(responseDetail);
+        //    }
+        //    return View(keyPointInfo);
+        //}
 
         // Get index in Json format
         [HttpGet]
